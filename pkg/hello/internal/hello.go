472aa2a99fc593f37803da81faf5678347961e74
mot <mot@pgmot.com>
2020-07-20 20:12:51 +0900

    update

--- pkg/hello/internal/hello.go
+++ pkg/hello/internal/hello.go
@@ -1,7 +1,13 @@
 package internal
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pgmot/awesomelib"
+)
 
 func InternalHello(name string) string {
+	data := awesomelib.NewHoge(name)
+	fmt.Println(data)
+
 	return fmt.Sprintf("Hello %s!", name)
 }
